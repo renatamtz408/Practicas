5d5cd37583fa8b5f447319183df43b5c0ed7285a
Renata Martinez <25430048@uttcampus.edu.mx>
2026-07-24 15:14:46 -0600

    clases main: modulos conexion y alumnos (creación)

---
+++ src/modulos/Conexion.java
@@ -0,0 +1,9 @@
+package modulos;
+
+public class Conexion {
+    private static final String URL = "jdbc:sqlserver://localhost:1433;databaseName=Alumnos;instanceName=SQLEXPRESS;trustServerCertificate=true;encrypt=false;";
+    private static final String USER = "Desarrollador";
+    private static final String PASSWORD = "1234";
+
+
+}
