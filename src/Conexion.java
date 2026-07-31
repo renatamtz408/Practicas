9cc2be519be8ebc4ecc8adeb7e47c02362fc7d11
Renata Martinez <25430048@uttcampus.edu.mx>
2026-07-31 13:55:31 -0600

    clase conexion.

---
+++ src/Conexion.java
@@ -0,0 +1,25 @@
+import java.sql.Connection;
+import java.sql.DriverManager;
+
+public class Conexion {
+    private static String server="localhost";
+    private static String port="1433";
+    private static String db="Paqueteria";
+    private static String user="Desarrolllador";
+    private static String psw="1234";
+
+    public static String getCadenaConexion(){
+        return String.format("jdbc:sqlserver://%s.%s;" +
+                "databaseName=%s;" +
+                "user=%s;" +
+                "password=%s;" +
+                "encrypt=true;" +
+                "trustServerCertificate=true;",
+                server,port,db,user,psw
+        );
+    }
+
+    public static Connection getConexion()throws Exception{
+        return DriverManager.getConnection(getCadenaConexion());
+    }
+}
